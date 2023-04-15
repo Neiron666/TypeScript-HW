400ad63da8cec619081e66930106653f556d3d1c
Valentyn <Neiron666@gmail.com>
2023-04-15 20:08:20 +0300

    update-15-04-2023

--- ts/app.ts
+++ ts/app.ts
@@ -53,115 +53,215 @@
 // };
 // console.log(card);
 
-//***********USER ARRAY*/
+//***********USER ARRAY**********/
 
-interface Name {
-  first: string;
-  midle?: string;
-  last: string;
-}
-interface Image {
-  url: string;
-  alt: string;
-}
+// interface Name {
+//   first: string;
+//   midle?: string;
+//   last: string;
+// }
+// interface Image {
+//   url: string;
+//   alt: string;
+// }
 
-interface Address {
-  state?: string;
-  country: string;
-  city: string;
-  street: string;
-  houseNumber: number;
-  ZIP: number;
-}
-interface User {
-  id: string;
-  name: Name;
-  phone: string;
-  email: string;
-  pasword: string;
-  image: Image;
-  address: Address;
-  isAdmin: boolean;
-  isBussiness: boolean;
-  createdAt: Date;
-}
+// interface Address {
+//   state?: string;
+//   country: string;
+//   city: string;
+//   street: string;
+//   houseNumber: number;
+//   ZIP: number;
+// }
+// interface User {
+//   id: string;
+//   name: Name;
+//   phone: string;
+//   email: string;
+//   pasword: string;
+//   image: Image;
+//   address: Address;
+//   isAdmin: boolean;
+//   isBussiness: boolean;
+//   createdAt: Date;
+// }
 
-type Combined = string | number | boolean;
+// type Combined = string | number | boolean;
 
-const tuple: [Combined, Combined] = [4, "sdfgdf"];
-const unionArray: (string | number)[] = [];
-const genericArray: Array<string | number> = [];
-const literalArray: (5 | 15)[] = [];
+// const tuple: [Combined, Combined] = [4, "sdfgdf"];
+// const unionArray: (string | number)[] = [];
+// const genericArray: Array<string | number> = [];
+// const literalArray: (5 | 15)[] = [];
 
-unionArray.push("Valik");
-unionArray.push("Valik");
-unionArray.push("Valik");
+// unionArray.push("Valik");
+// unionArray.push("Valik");
+// unionArray.push("Valik");
 
-genericArray.push("BBB");
-genericArray.push("AAA");
-genericArray.push("RRR");
+// genericArray.push("BBB");
+// genericArray.push("AAA");
+// genericArray.push("RRR");
 
-genericArray.forEach((element) => {
-  console.log(element);
-});
+// genericArray.forEach((element) => {
+//   console.log(element);
+// });
 
-const users: User[] = [
-  {
-    id: "123",
-    name: {
-      first: "string",
-      midle: "string",
-      last: "string",
-    },
-    phone: "string",
-    email: "string",
-    pasword: "string",
-    image: {
-      url: "string",
-      alt: "string",
-    },
-    address: {
-      state: "string",
-      country: "string",
-      city: "string",
-      street: "string",
-      houseNumber: 5,
-      ZIP: 5,
-    },
-    isAdmin: false,
-    isBussiness: false,
-    createdAt: new Date(),
-  },
-  {
-    id: "123",
-    name: {
-      first: "string",
-      midle: "string",
-      last: "string",
-    },
-    phone: "string",
-    email: "string",
-    pasword: "string",
-    image: {
-      url: "string",
-      alt: "string",
-    },
-    address: {
-      state: "string",
-      country: "string",
-      city: "string",
-      street: "string",
-      houseNumber: 5,
-      ZIP: 5,
-    },
-    isAdmin: false,
-    isBussiness: false,
-    createdAt: new Date(),
-  },
-];
+// const users: User[] = [
+//   {
+//     id: "123",
+//     name: {
+//       first: "string",
+//       midle: "string",
+//       last: "string",
+//     },
+//     phone: "string",
+//     email: "string",
+//     pasword: "string",
+//     image: {
+//       url: "string",
+//       alt: "string",
+//     },
+//     address: {
+//       state: "string",
+//       country: "string",
+//       city: "string",
+//       street: "string",
+//       houseNumber: 5,
+//       ZIP: 5,
+//     },
+//     isAdmin: false,
+//     isBussiness: false,
+//     createdAt: new Date(),
+//   },
+//   {
+//     id: "123",
+//     name: {
+//       first: "string",
+//       midle: "string",
+//       last: "string",
+//     },
+//     phone: "string",
+//     email: "string",
+//     pasword: "string",
+//     image: {
+//       url: "string",
+//       alt: "string",
+//     },
+//     address: {
+//       state: "string",
+//       country: "string",
+//       city: "string",
+//       street: "string",
+//       houseNumber: 5,
+//       ZIP: 5,
+//     },
+//     isAdmin: false,
+//     isBussiness: false,
+//     createdAt: new Date(),
+//   },
+// ];
+// const user1 = {
+//   id: "123",
+//   name: {
+//     first: "string",
+//     midle: "string",
+//     last: "string",
+//   },
+//   phone: "string",
+//   email: "string",
+//   pasword: "string",
+//   image: {
+//     url: "string",
+//     alt: "string",
+//   },
+//   address: {
+//     state: "string",
+//     country: "string",
+//     city: "string",
+//     street: "string",
+//     houseNumber: 5,
+//     ZIP: 5,
+//   },
+//   isAdmin: false,
+//   isBussiness: false,
+//   createdAt: new Date(),
+// };
+// const user2 = {
+//   id: "12",
+//   name: {
+//     first: "string",
+//     midle: "string",
+//     last: "string",
+//   },
+//   phone: "string",
+//   email: "string",
+//   pasword: "string",
+//   image: {
+//     url: "string",
+//     alt: "string",
+//   },
+//   address: {
+//     state: "string",
+//     country: "string",
+//     city: "string",
+//     street: "string",
+//     houseNumber: 5,
+//     ZIP: 5,
+//   },
+//   isAdmin: false,
+//   isBussiness: false,
+//   createdAt: new Date(),
+// };
+// const user3 = {
+//   id: "1234",
+//   name: {
+//     first: "string",
+//     midle: "string",
+//     last: "string",
+//   },
+//   phone: "string",
+//   email: "string",
+//   pasword: "string",
+//   image: {
+//     url: "string",
+//     alt: "string",
+//   },
+//   address: {
+//     state: "string",
+//     country: "string",
+//     city: "string",
+//     street: "string",
+//     houseNumber: 5,
+//     ZIP: 5,
+//   },
+//   isAdmin: false,
+//   isBussiness: false,
+//   createdAt: new Date(),
+// };
+// const fn = (id: string, users: User[]) => {
+//   if (!users.length) return null;
+//   const goodId = users.filter((item) => {
+//     return item.id === id;
+//     // console.log(item.id, id);
+//   });
 
+//   if (!goodId.length) return "No user with this id was found";
+//   return goodId;
+// };
 
-const fn = (id: string, users: User[]) => {
-  if (!users) return null;
-  if 
-};
+// const fn1 = fn("123", [user1]);
+// const fn2 = fn("123", [user2]);
+// const fn3 = fn("123", [user3]);
+
+// console.log(fn1);
+// console.log(fn2);
+// console.log(fn3);
+
+//***********CLASS**********/
+
+// class User {
+//   #id: number;
+//   #name: string;
+//   #address:object;
+//   constructor(){}
+
+// }
